8d4e640998a26b17f1d183e34479b7e32d31c905
DiegoArturoMG <diegoarturo2121@gmail.com>
2019-02-13 10:03:58 +0800

    Agregue funcion

--- prueba.c
+++ prueba.c
@@ -18,23 +18,15 @@ int hola(int);
 
 int main(int argc, char const *argv[]){
 	printf("Hola\n");
-
-	printf("HOla 2_ BRYAN\n");
-	printf("hahaha\n");
-	printf("HOla 2\n");
-
-	printf("hola4\n");
-	printf("hola 5\n");
-
-	printf("hola 6\n");
-	printf("hola 7\n");
 	return 0;
-}
-
-int hola(int a){
-	printf("%d\n",a);
 }
 
 int hola2(){
 	printf("hahaha\n");
+}
+
+void bryanSeLaCome(){
+	for(;;){
+		printf("jajaja\n");
+	}
 }
