4127430f0b5e371e891ae314e6796bbdcd4ea608
bryanblancas <orionmunecaycanica@gmail.com>
2019-02-12 19:33:06 -0600

    prueba

--- prueba.c
+++ prueba.c
@@ -2,19 +2,13 @@
 int main(int argc, char const *argv[]){
 	printf("Hola\n");
 
-<<<<<<< HEAD
 	printf("HOla 2_ BRYAN\n");
 	printf("hahaha\n");
-=======
 	printf("HOla 2\n");
 
 	printf("hola4\n");
 	printf("hola 5\n");
-<<<<<<< HEAD
 
 	printf("hola 6\n");
-=======
->>>>>>> 9c20496f3bce0a45c8b43937caddec239871e510
->>>>>>> 261648032778faac1909df601b10909b8775a898
 	return 0;
 }
